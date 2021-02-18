8eab6f707adf0410c62063ac7ba0606113f9318c
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-02-18 15:01:13 +0100

    Update Automa

--- crud-ill/src/crud/ill/AutomaCrud.java
+++ crud-ill/src/crud/ill/AutomaCrud.java
@@ -10,10 +10,72 @@ package crud.ill;
  * @author Utente
  */
 public class AutomaCrud implements State {
-    
+
+    private State stato;
+
+    public AutomaCrud() {
+        stato = new Ricerca();
+    }
+
     @Override
     public void next(Event e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        stato.next(e);
     }
-    
+
+    private class Ricerca implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof RicercaEvent) {
+            } else if (e instanceof AddEvent) {
+                stato = new Aggiungi();
+            } else if (e instanceof SelezionaEvent) {
+                stato = new Visualizza();
+            } else {
+                System.out.println("Evento inaspettato.");
+            }
+        }
+    }
+
+    private class Aggiungi implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof ConfermaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof AnnullaEvent) {
+                stato = new Ricerca();
+            } else {
+                System.out.println("Evento inaspettato.");
+            }
+        }
+    }
+
+    private class Visualizza implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof SelezionaEvent) {
+            } else if (e instanceof ModificaEvent) {
+                stato = new Modifica();
+            }
+        }
+    }
+
+    private class Modifica implements State {
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    private class Rimuovi implements State {
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
 }
